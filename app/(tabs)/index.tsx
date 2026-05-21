5cf9ffb206ff7f2b51957b651ee211604ac1ef68
Terry Parrish <heymonheymon3000@yahoo.com>
2026-05-21 08:36:29 -0400

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,34 @@
-import "@/global.css"
+import "@/global.css";
 import { Text } from "react-native";
-import {Link} from "expo-router";
+import { Link } from "expo-router";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 import { styled } from "nativewind";
+
 const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
-    return (
-        <SafeAreaView className="flex-1 bg-background p-5">
-            <Text className="text-xl font-bold text-success">
-                Welcome to Native wind!
-            </Text>
-            <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">Go to onboarding</Link>
-            <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">Go to Sign In</Link>
-            <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Go to Sign UP</Link>
-            <Link href="/subscriptions/spotify">Spotify Subscription</Link>
-            <Link href={{
-                pathname: "/[id]",
-                params: {id: "Claude"},
-            }}
-            >
-                Claude Max Subscription
-            </Link>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <Text className="text-5xl font-sans-extrabold text-primary">Home</Text>
+
+      <Link
+        href="/onboarding"
+        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
+      >
+        Go to onboarding
+      </Link>
+      <Link
+        href="/(auth)/sign-in"
+        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
+      >
+        Go to Sign In
+      </Link>
+      <Link
+        href="/(auth)/sign-up"
+        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
+      >
+        Go to Sign UP
+      </Link>
+    </SafeAreaView>
+  );
 }
