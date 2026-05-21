035b8fee56d9ea7d47ea8700b994dcfd6d058f26
Terry Parrish <heymonheymon3000@yahoo.com>
2026-05-21 16:00:28 -0400

    feat: clerk auth implementation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,16 @@ import { SplashScreen, Stack } from "expo-router";
 import "@/global.css";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
+import { ClerkProvider } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache";
 
 SplashScreen.preventAutoHideAsync();
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error("Add your Clerk Publishable Key to the .env file");
+}
 
 export default function RootLayout() {
   const [fontsLoaded, fontError] = useFonts({
@@ -24,5 +32,9 @@ export default function RootLayout() {
   if (!fontsLoaded && !fontError) return null;
   if (fontError) throw fontError;
 
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </ClerkProvider>
+  );
 }
